db2a621f757caa65c6717a6cec5b81eec7de222d
Pavel Tkachenko <paveltkachenko.gml@gmail.com>
2020-04-25 20:04:59 +0700

    commit

--- src/main/java/net/dto/IssueType.java
+++ src/main/java/net/dto/IssueType.java
@@ -1,16 +1,15 @@
 package net.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
-import net.jackson.deserializer.IssueTypeDeserializer;
 
 /**
  * Issue type.
  *
  * @version 0.1
  */
-@JsonDeserialize(using = IssueTypeDeserializer.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class IssueType extends Resource {
   @Getter @Setter private String name;
   @Getter @Setter private String description;
